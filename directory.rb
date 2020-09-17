3e674db4d705aa7845ebb69a3619bebf2b8137bc
Dan Holmes <dh@danielholmes.com>
2020-09-17 14:55:00 +0100

    refactoring

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+@students = []
+
 def puts_center(text)
   page_width = 80
   puts text.center(page_width)
@@ -6,10 +8,9 @@ end
 def input_students
   puts "Enter students one by one, followed by their country of origin and cohort"
   puts "Enter an empty line when you're done"
-  students = []
   while true do
     input = gets.delete("\n") # Modified from gets.chomp to satisfy excercise 10
-    return students if input.empty?
+    break if input.empty?
     name = input
     puts "Enter country:"
     country = gets.chomp
@@ -17,8 +18,8 @@ def input_students
     cohort = gets.chomp
     cohort = "November" if cohort.empty?
     cohort = cohort.to_sym
-    students << ({name: input, cohort: cohort, country: country})
-    puts "Now we have #{students.count} students"
+    @students << ({name: input, cohort: cohort, country: country})
+    puts "Now we have #{@students.count} students"
   end
 end
 
@@ -27,12 +28,12 @@ def print_header
   puts_center("-------------")
 end
 
-def print(students)
-  return if students.count == 0 # Do not try and print the list if it's empty
-  cohorts = get_cohorts(students)
+def print_students
+  return if @students.count == 0 # Do not try and print the list if it's empty
+  cohorts = get_cohorts
   cohorts.each { |cohort|
     puts_center "#{cohort} cohort:"
-    students.each { |student|
+    @students.each { |student|
       if student[:cohort] == cohort
         puts_center("#{student[:name]} from #{student[:country]}")
       end
@@ -40,46 +41,53 @@ def print(students)
   }
 end
 
-def get_cohorts(students)
-  return students.map {|student| student[:cohort]}.uniq
+def get_cohorts
+  @students.map {|student| student[:cohort]}.uniq
 end
 
-def filter_by_letter(students, letter)
+def filter_by_letter(letter)
   filtered_students = []
-  students.each { |student|
+  @students.each { |student|
     filtered_students << student if student[:name][0] == letter
   }
   filtered_students
 end
 
-def filter_less_than_12(students)
+def filter_less_than_12
   filtered_students = []
-  students.each { |student|
+  @students.each { |student|
     filtered_students << student if student[:name].length < 12
   }
   filtered_students
 end
 
-def print_footer(students)
+def print_footer
   puts ""
-  students.count == 1 ? plural_modifier = "" : plural_modifier = "s"
-  puts_center("Overall we have #{students.count} great student#{plural_modifier}")
+  @students.count == 1 ? plural_modifier = "" : plural_modifier = "s"
+  puts_center("Overall we have #{@students.count} great student#{plural_modifier}")
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     selection = gets.chomp
     case selection
     when "1"
-      students = input_students()
+      input_students
     when "2"
-      print_header()
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
@@ -88,4 +96,4 @@ def interactive_menu
   end
 end
 
-interactive_menu()
+interactive_menu
