91c6f302025b800ecf89259a6cef3ed0d8c21193
Dan Holmes <dh@danielholmes.com>
2020-09-17 11:25:15 +0100

    Add excercises 1-6

--- directory.rb
+++ directory.rb
@@ -1,32 +1,57 @@
+def puts_center(text)
+  page_width = 80
+  puts text.center(page_width)
+end
+
 def input_students
-  puts "Enter students one by one"
+  puts "Enter students one by one, followed by their country of origin"
   puts "Enter an empty line when you're done"
   students = []
   while true do
     input = gets.chomp
     return students if input.empty?
-    students << ({name: input, cohort: :november})
+    name = input
+    puts "Enter country:"
+    country = gets.chomp
+    students << ({name: input, cohort: :november, country: country})
     puts "Now we have #{students.count} students"
   end
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts_center("The students of Villains Academy")
+  puts_center("-------------")
 end
 
 def print(students)
   students.each_with_index do |student, index|
     list_number = index + 1
-    puts "#{list_number}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts_center("#{list_number}. #{student[:name]} from #{student[:country]} (#{student[:cohort]} cohort)")
   end
 end
 
+def filter_by_letter(students, letter)
+  filtered_students = []
+  students.each { |student|
+    filtered_students << student if student[:name][0] == letter
+  }
+  filtered_students
+end
+
+def filter_less_than_12(students)
+  filtered_students = []
+  students.each { |student|
+    filtered_students << student if student[:name].length < 12
+  }
+  filtered_students
+end
+
 def print_footer(students)
-  puts "Overall we have #{students.count} great students"
+  puts ""
+  puts_center("Overall we have #{students.count} great students")
 end
 
 students = input_students
 print_header
-print(students)
+print(filter_less_than_12(filter_by_letter(students,'D')))
 print_footer(students)
