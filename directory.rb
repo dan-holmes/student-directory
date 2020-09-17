9a8b4f7872e1b95b94219d48fc5524dbebc73ff8
Dan Holmes <dh@danielholmes.com>
2020-09-17 16:11:39 +0100

    more refactoring

--- directory.rb
+++ directory.rb
@@ -79,20 +79,23 @@ def show_students
   print_footer
 end
 
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def interactive_menu
   loop do
     print_menu
-    selection = gets.chomp
-    case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    process(gets.chomp)
   end
 end
 
